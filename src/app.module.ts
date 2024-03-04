3dc0ac04ed2541b9c1f0429dac59a276141a0181
ibrahim <ibrahim@addissoftware.com>
2024-03-04 14:51:08 +0300

    ft: end-points for getting data.

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { [cats]Controller } from './[cats]/[cats].controller';
 import { CatsController } from './cats/cats.controller';
 
 @Module({
   imports: [],
-  controllers: [AppController, [cats]Controller, CatsController],
+  controllers: [AppController, CatsController],
   providers: [AppService],
 })
 export class AppModule {}
--- src/cats/cats.controller.ts
+++ src/cats/cats.controller.ts
@@ -1,9 +1,46 @@
-import { Controller, Get } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Header,
+  Param,
+  Post,
+  Query,
+  Redirect,
+  Req,
+} from '@nestjs/common';
+import { Request } from 'express';
 
 @Controller('cats')
 export class CatsController {
+  @Post()
+  @Header('Cache-Control', 'none')
+  create(): string {
+    return 'This action adds a new cat';
+  }
+
   @Get()
-  findAll(): string {
+  @Redirect('https://nestjs.com', 301)
+  findAll(@Req() request: Request): string {
     return 'This action returns all cats';
+  }
+
+  @Get('docs')
+  // @Redirect('https://docs.nestjs.com', 302)
+  getDocs(@Query() version: number) {
+    console.log(version);
+    if (version && version === 5) {
+      return { url: 'https://docs.nestjs.com/v5/' };
+    }
+  }
+
+  @Get('ab*cd')
+  findAlls() {
+    return 'This route uses a wildcard';
+  }
+
+  @Get(':id')
+  findOne(@Param() params: any): string {
+    console.log(params.id);
+    return `This action returns a #${params.id} cat`;
   }
 }
