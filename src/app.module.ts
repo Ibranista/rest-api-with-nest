8a5b766d8f10c8caf5debba63f17cebef9f406d7
ibrahim <ibrahim@addissoftware.com>
2024-03-04 13:51:36 +0300

    cats route.

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { [cats]Controller } from './[cats]/[cats].controller';
+import { CatsController } from './cats/cats.controller';
 
 @Module({
   imports: [],
-  controllers: [AppController],
+  controllers: [AppController, [cats]Controller, CatsController],
   providers: [AppService],
 })
 export class AppModule {}
